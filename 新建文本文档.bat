cfe78a2746db4d3d49eaad8a3358aac3864a7dc5
ygj2295 <ygj18838232295@outlook.com>
2025-06-27 20:54:11 +0800

    Initial commit

--- 新建文本文档.bat
+++ 新建文本文档.bat
@@ -1,5 +1,7 @@
 git init
+
 git remote add origin https://github.com/ygj2295/ygj2295.github.io.git
 git add .
 git commit -m "Initial commit"
+git -c http.sslVerify=false push origin main
 git push -u origin main
